26ac4bbb9dbebb64c4820727a5f7a483fdb8d497
OwnDie <python@localhost.localdomain>
2019-06-28 07:53:38 -0400

    Ex 5.1 done.

--- ex5_1.py
+++ ex5_1.py
@@ -1,20 +1,29 @@
+from sys import argv
 
+network, mask_decimial = argv[1:]
 
+#ip_address = input('Please input network and mask (example 10.1.1.0/24): ')
 
-ip_address = input('Please input network and mask (example 10.1.1.0/24): ')
-
-ip_address_out = ip_address.split('/')
-network = ip_address_out[0].split('.')
-mask_decimial = int(ip_address_out[1])
+#ip_address_out = ip_address.split('/')
+network = network.split('.')
+mask_decimial = int(mask_decimial)
 
 network[0] = int(network[0])
 network[1] = int(network[1])
 network[2] = int(network[2])
 network[3] = int(network[3])
 
+network_temp = '{:08b}'.format(network[0])+'{:08b}'.format(network[1])+'{:08b}'.format(network[2])+'{:08b}'.format(network[3])
+print(network_temp)
+network_temp = network_temp[0:mask_decimial] + '0'*(32 - mask_decimial)
+
+print(network_temp)
+
+network = [int(network_temp[0:8],2),int(network_temp[8:16],2),int(network_temp[16:24],2),int(network_temp[24:32],2)]
+
 print('''Network:
 {0:<8}  {1:<8}  {2:<8}  {3:<8}
-{0:<08b}  {1:<08b}  {2:<08b}  {3:<08b}\n'''.format(network[0],network[1],network[2],network[3]))
+{0:08b}  {1:08b}  {2:08b}  {3:08b}\n'''.format(network[0],network[1],network[2],network[3]))
 
 mask = '1' * mask_decimial + '0' * (32 - mask_decimial)
 mask_list = [int(mask[0:8],2),int(mask[8:16],2),int(mask[16:24],2),int(mask[24:32],2)]
